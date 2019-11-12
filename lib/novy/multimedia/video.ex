0628e9b4ab98473c8ee0fc3279cd101465af074b
Aymeric <aymeric.legros@adixon.fr>
2019-11-12 18:00:22 +0100

    first commit

---
+++ lib/novy/multimedia/video.ex
@@ -0,0 +1,23 @@
+defmodule Novy.Multimedia.Video do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "videos" do
+    field :description, :string
+    field :title, :string
+    field :url, :string
+    # field :user_id, :id
+    belongs_to :user, Novy.Accounts.User
+    belongs_to :category, Novy.Multimedia.Category
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(video, attrs) do
+    video
+    |> cast(attrs, [:url, :title, :description, :category_id])
+    |> validate_required([:url, :title, :description])
+    |> assoc_constraint(:category)
+  end
+end
---
+++ lib/novy_web/router.ex
@@ -0,0 +1,35 @@
+defmodule NovyWeb.Router do
+  use NovyWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug NovyWeb.Auth
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", NovyWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
+  end
+
+  scope "/manage", NovyWeb do
+    pipe_through [:browser, :authenticate_user]
+
+    resources "/videos", VideoController
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", NovyWeb do
+  #   pipe_through :api
+  # end
+end
